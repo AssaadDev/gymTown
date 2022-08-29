121a1bdff84016dcae1284171dbc6431e3ea0ebf
Ahmed <78742811+AssaadDev@users.noreply.github.com>
2022-08-30 00:46:38 +0200

    Update ApiSpec.php
    test

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -5,7 +5,7 @@
  * @OA\Info(title="gymTown API Specs", version="0.2", @OA\Contact(email="ahmed.assaad@stu.ibu.edu.ba", name="Ahmed Assaad"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost:8080/gymtown/rest", description="Development Environment" ),
- *    @OA\Server(url="https://gymtown.herokuapp.com/rest", description="Production Environment" )
+ *    @OA\Server(url="https://gymtown.herokuapp.com/rest/", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
